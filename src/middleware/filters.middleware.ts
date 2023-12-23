28520ea0b34cd6eba5f00bec16c28b4957520a3c
Vincent Lin <vinlin24@outlook.com>
2023-12-23 14:35:39 +0800

    Move UID mapping to a new utils file

--- src/middleware/filters.middleware.ts
+++ src/middleware/filters.middleware.ts
@@ -1,16 +1,10 @@
 import { Events } from "discord.js";
 
-import config from "../config";
 import { ListenerFilter } from "../types/listener.types";
-
-// TODO: probably belongs in another file.
-const KEY_TO_UIDS = {
-  "LUKE": config.LUKE_UID,
-  "KLEE": config.KLEE_UID,
-};
+import uids from "../utils/uids.utils";
 
 export function messageFrom(
-  key: keyof typeof KEY_TO_UIDS,
+  key: keyof typeof uids,
 ): ListenerFilter<Events.MessageCreate> {
-  return message => message.author.id === KEY_TO_UIDS[key];
+  return message => message.author.id === uids[key];
 }
---
+++ src/utils/uids.utils.ts
@@ -0,0 +1,18 @@
+import config from "../config";
+
+/**
+ * Mapping of "name" to Discord user ID, useful for user-specific features and
+ * functionality. While the values for the UIDs are provided in the environment
+ * file, this mapping provides a way to use names as a key type.
+ *
+ * WARNING: Use a nickname or other alias where possible to avoid possible leaks
+ * of PII. First names, especially common ones, are probably okay. Do not under
+ * any circumstances use legal or full names as the keys!! This applies to
+ * naming the corresponding environment variables too.
+ */
+const NAME_TO_UID = {
+  "LUKE": config.LUKE_UID,
+  "KLEE": config.KLEE_UID,
+};
+
+export default NAME_TO_UID;
