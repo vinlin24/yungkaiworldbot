3f18bcc674d5a179a78aaf48a47c008be7130917
Vincent Lin <vinlin24@outlook.com>
2023-12-24 16:40:17 +0800

    Convert Cxtie controller to MessageListener

--- src/controllers/special/cxtie.controller.ts
+++ src/controllers/special/cxtie.controller.ts
@@ -1,20 +1,34 @@
-import { Events } from "discord.js";
-
+import getLogger from "../../logger";
 import { messageFrom } from "../../middleware/filters.middleware";
-import cxtieService from "../../services/cxtie.service";
-import { Controller, Listener } from "../../types/controller.types";
+import { Controller, MessageListener } from "../../types/controller.types";
+import { replySilently } from "../../utils/interaction.utils";
+import { formatContext } from "../../utils/logging.utils";
 
-const onMessage = new Listener<Events.MessageCreate>({
-  name: Events.MessageCreate,
+const log = getLogger(__filename);
+
+const onSniffs = new MessageListener()
+
+onSniffs.filter(messageFrom("CXTIE"));
+onSniffs.filter(message => {
+  const sniffsWithPossibleMarkdown = /^(?:[*_`|~]*|#+ )sniffs?[*_`|~]*$/i;
+  return !!message.content.match(sniffsWithPossibleMarkdown);
 });
 
-onMessage.filter(messageFrom("CXTIE"));
-onMessage.execute(cxtieService.processSniffs);
+onSniffs.cooldown.set({
+  type: "global",
+  seconds: 600,
+});
+
+onSniffs.execute(async (message) => {
+  await replySilently(message, message.content);
+  const context = formatContext(message);
+  log.info(`${context}: echoed sniffs.`);
+});
 
 const controller: Controller = {
   name: "cxtie",
   commands: [],
-  listeners: [onMessage],
+  listeners: [onSniffs],
 };
 
 export default controller;
