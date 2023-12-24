a3eb09dab27cb6f808c4621500fee2fefe7aacaf
Vincent Lin <vinlin24@outlook.com>
2023-12-24 13:23:35 +0800

    Convert Klee controller to MessageListener w/ CD

--- src/controllers/special/klee.controller.ts
+++ src/controllers/special/klee.controller.ts
@@ -1,13 +1,28 @@
-import { Events } from "discord.js";
 
-import { channelPollutionAllowed, ignoreBots } from "../../middleware/filters.middleware";
-import kleeService from "../../services/klee.service";
-import { Controller, Listener } from "../../types/controller.types";
+import getLogger from "../../logger";
+import {
+  channelPollutionAllowed,
+  ignoreBots,
+} from "../../middleware/filters.middleware";
+import { Controller, MessageListener } from "../../types/controller.types";
+import { replySilently } from "../../utils/interaction.utils";
+import { formatContext } from "../../utils/logging.utils";
 import uids from "../../utils/uids.utils";
 
-const onDab = new Listener<Events.MessageCreate>({
-  name: Events.MessageCreate,
+const log = getLogger(__filename);
+
+const onDab = new MessageListener();
+
+onDab.cooldown({
+  type: "dynamic",
+  defaultSeconds: 600,
 });
+
+if (uids.KLEE === undefined) {
+  log.warn("klee UID not found.");
+} else {
+  onDab.setCooldown(0, uids.KLEE); // Klee dab can bypass cooldown.
+}
 
 onDab.filter(ignoreBots);
 onDab.filter(message => // Klee's dab can bypass channel restrictions.
@@ -17,7 +32,11 @@ onDab.filter(message =>
   message.content.toLowerCase() === "dab"
 );
 
-onDab.execute(kleeService.dabBack);
+onDab.execute(async (message) => {
+  await replySilently(message, "dab");
+  const context = formatContext(message);
+  log.debug(`${context}: dabbed back.`);
+});
 
 const controller: Controller = {
   name: "klee",
