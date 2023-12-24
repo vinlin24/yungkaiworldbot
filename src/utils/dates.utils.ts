be0e7be97a72c898e459edb0f3d44d2034243339
Vincent Lin <vinlin24@outlook.com>
2023-12-24 22:05:42 +0800

    Add some Date and Discord markdown-related utils

--- src/utils/dates.utils.ts
+++ src/utils/dates.utils.ts
@@ -7,3 +7,46 @@ export function addDateSeconds(date: Date, seconds: number): Date {
   newDate.setSeconds(newDate.getSeconds() + seconds);
   return newDate;
 }
+
+export function toUnixSeconds(date: Date): number {
+  // The underlying number in JavaScript Dates is in milliseconds.
+  const seconds = Math.floor(Date.now() / 1000);
+  return seconds;
+}
+
+export function formatHoursMinsSeconds(seconds: number): string {
+  function formatHours(): string {
+    const hours = Math.floor(seconds / 3600);
+    if (hours > 0) {
+      const unit = hours === 1 ? "hour" : "hours";
+      return `${hours} ${unit}`;
+    }
+    return "";
+  }
+
+  function formatMinutes(): string {
+    const remainingMinutes = Math.floor((seconds % 3600) / 60);
+    if (remainingMinutes > 0) {
+      const unit = remainingMinutes === 1 ? "minute" : "minutes";
+      return `${remainingMinutes} ${unit}`;
+    }
+    return "";
+  }
+
+  function formatSeconds(): string {
+    const remainingSeconds = seconds % 60;
+    if (remainingSeconds > 0) {
+      const unit = remainingSeconds === 1 ? "second" : "seconds";
+      return `${remainingSeconds} ${unit}`;
+    }
+    return "";
+  }
+
+  const formattedTime = [
+    formatHours(),
+    formatMinutes(),
+    formatSeconds(),
+  ].filter(Boolean).join(", ");
+
+  return formattedTime || "0 seconds";
+}
--- src/utils/markdown.utils.ts
+++ src/utils/markdown.utils.ts
@@ -1,17 +1,65 @@
 // Also see:
 // https://v13.discordjs.guide/miscellaneous/parsing-mention-arguments.html
 
-export function toRoleMention(roleId: string) {
+import { toUnixSeconds } from "./dates.utils";
+
+export function toRoleMention(roleId: string): string {
   return `<@&${roleId}>`;
 }
 
-export function toUserMention(userId: string) {
+export function toUserMention(userId: string): string {
   return `<@${userId}>`;
 }
 
-export function toChannelMention(channelId: string) {
+export function toChannelMention(channelId: string): string {
   return `<#${channelId}>`;
 }
 
-// This module can also be expanded to include helpers for parsing mentions,
-// creating timestamps, etc.
+export function joinUserMentions(userIds?: Iterable<string>): string {
+  if (!userIds) return "";
+  return Array.from(userIds).map(toUserMention).join(", ");
+}
+
+/**
+ * See: https://gist.github.com/LeviSnoot/d9147767abeef2f770e9ddcd91eb85aa.
+ */
+export const enum TimestampFormat {
+  /** 12-hour Example: 9:01 AM */
+  SHORT_TIME = "t",
+  /** 12-hour Example: 9:01:00 AM */
+  LONG_TIME = "T",
+  /** 12-hour Example: 11/28/2018 */
+  SHORT_DATE = "d",
+  /** 12-hour Example: November 28, 2018 */
+  LONG_DATE = "D",
+  /** 12-hour Example: November 28, 2018 9:01 AM */
+  SHORT_DATETIME = "f",
+  /** 12-hour Example: Wednesday, November 28, 2018 9:01 AM */
+  LONG_DATETIME = "F",
+  /** 12-hour Example: 3 years ago */
+  RELATIVE = "R",
+}
+
+export function toTimestampMention(
+  date: Date,
+  format?: TimestampFormat,
+): string {
+  const unixTimestamp = toUnixSeconds(date);
+  let result = `<t:${unixTimestamp}`;
+  if (format) {
+    result += `:${format}`;
+  }
+  result += ">";
+  return result;
+}
+
+export function toRelativeTimestampMention(date: Date): string {
+  // I assume this format is among the most common ones besides the default one,
+  // so it's nice to have a shorthand function partial.
+  return toTimestampMention(date, TimestampFormat.RELATIVE);
+}
+
+export function toBulletedList(lines: string[], level: number = 0): string {
+  const indent = "  ".repeat(level);
+  return lines.map(line => `${indent}* ${line}`).join("\n");
+}
