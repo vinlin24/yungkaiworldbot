2063ae579d832a9d8444b9919bb243079ed996ea
Vincent Lin <vinlin24@outlook.com>
2023-12-28 20:46:48 +0800

    Add missing content filters from gulp and mwah

--- src/controllers/special/misc.controller.ts
+++ src/controllers/special/misc.controller.ts
@@ -1,10 +1,14 @@
-import { messageFrom } from "../../middleware/filters.middleware";
+import {
+  contentMatching,
+  messageFrom,
+} from "../../middleware/filters.middleware";
 import { Controller, MessageListener } from "../../types/controller.types";
 import { replySilentlyWith } from "../../utils/interaction.utils";
 
 const onGulp = new MessageListener("gulp");
 
 onGulp.filter(messageFrom("BUNNY"));
+onGulp.filter(contentMatching(/^gulp$/i));
 onGulp.cooldown.set({
   type: "global",
   seconds: 600,
@@ -14,6 +18,7 @@ onGulp.execute(replySilentlyWith("gulp"));
 const onMwah = new MessageListener("mwah");
 
 onMwah.filter(messageFrom("J"));
+onMwah.filter(contentMatching(/^mwah$/i));
 onMwah.cooldown.set({
   type: "global",
   seconds: 600,
