e27ac0b397fae2bb4779e8d7544ab3f299817d5d
Vincent Lin <vinlin24@outlook.com>
2023-12-28 14:49:42 +0800

    Add "mwah" and "gulp" requested features

---
+++ src/controllers/special/misc.controller.ts
@@ -0,0 +1,29 @@
+import { messageFrom } from "../../middleware/filters.middleware";
+import { Controller, MessageListener } from "../../types/controller.types";
+import { replySilentlyWith } from "../../utils/interaction.utils";
+
+const onGulp = new MessageListener("gulp");
+
+onGulp.filter(messageFrom("BUNNY"));
+onGulp.cooldown.set({
+  type: "global",
+  seconds: 600,
+});
+onGulp.execute(replySilentlyWith("gulp"));
+
+const onMwah = new MessageListener("mwah");
+
+onMwah.filter(messageFrom("J"));
+onMwah.cooldown.set({
+  type: "global",
+  seconds: 600,
+});
+onMwah.execute(replySilentlyWith("mwah"));
+
+const controller: Controller = {
+  name: "misc",
+  commands: [],
+  listeners: [onMwah],
+};
+
+export default controller;
