3c75f4ee3e820d8fa6e426c921d59cfd11ed8f72
Vincent Lin <vinlin24@outlook.com>
2023-12-31 12:48:31 +0800

    Reply with Lee Gwan Hee picture on Kai saying name

---
+++ src/controllers/users/kai/lee-gwan-hee.listener.ts
@@ -0,0 +1,31 @@
+import path from "node:path";
+
+import { MessageFlags } from "discord.js";
+
+import {
+  contentMatching,
+  messageFrom,
+} from "../../../middleware/filters.middleware";
+import { MessageListenerBuilder } from "../../../types/listener.types";
+
+// TODO: Make assets path a global configuration constant.
+const LEE_GWAN_HEE_PATH = path.join(
+  __dirname, "..", "..", "..", "assets", "lee-gwan-hee.jpg",
+);
+
+const LEE_GWAN_HEE_REGEXP = /lee ?[gk]wan[ -]?hee/i;
+
+const onLeeGwanHee = new MessageListenerBuilder().setId("lee-gwan-hee");
+
+onLeeGwanHee.filter(messageFrom("KAI"));
+onLeeGwanHee.filter(contentMatching(LEE_GWAN_HEE_REGEXP));
+onLeeGwanHee.execute(async (interaction) => {
+  await interaction.reply({
+    files: [LEE_GWAN_HEE_PATH],
+    allowedMentions: { parse: [] },
+    flags: MessageFlags.SuppressNotifications,
+  });
+});
+
+const onLeeGwanHeeSpec = onLeeGwanHee.toSpec();
+export default onLeeGwanHeeSpec;
