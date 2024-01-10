2b8d87c25649bb9ac9a24370c87f663a7ebfb75b
Vincent Lin <vinlin24@outlook.com>
2024-01-10 13:15:14 -0800

    Echo Cxtie sniffs emoji half the time
    The other half remains the "daily cxtie appreciation" feature. It's just
    that now the listener will fire on two triggers:
    * The existing "sniffs" with possible Markdown trigger
    * Content containing the custom Cxtie sniffs emoji

--- tests/controllers/users/cxtie/sniffs.listener.test.ts
+++ tests/controllers/users/cxtie/sniffs.listener.test.ts
@@ -1,11 +1,18 @@
 import config from "../../../../src/config";
 import onSniffsSpec from "../../../../src/controllers/users/cxtie/sniffs.listener";
+import { GUILD_EMOJIS, toEscapedEmoji } from "../../../../src/utils/emojis.utils";
 import { MockMessage } from "../../../test-utils";
 
 describe("sniffs listener", () => {
   afterEach(() => {
     jest.spyOn(global.Math, "random").mockRestore();
   })
+
+  it("should only listen to Cxtie", async () => {
+    const mock = new MockMessage(onSniffsSpec).mockContent("i'm not cxtie");
+    await mock.simulateEvent();
+    mock.expectNotResponded();
+  });
 
   it("should echo sniffs half the time", async () => {
     const mock = new MockMessage(onSniffsSpec)
@@ -23,5 +30,28 @@ describe("sniffs listener", () => {
     jest.spyOn(global.Math, "random").mockReturnValueOnce(0);
     await mock.simulateEvent();
     mock.expectRepliedSilentlyWith({ content: "daily cxtie appreciation" });
+  });
+
+  it("should echo the Cxtie sniffs emojis if present", async () => {
+    const sniffsEmoji = toEscapedEmoji({
+      name: "some-name",
+      id: GUILD_EMOJIS.CXTIE_SNIFFS,
+    });
+    const mock = new MockMessage(onSniffsSpec)
+      .mockContent(`You ${sniffsEmoji} make me cry ${sniffsEmoji}!`)
+      .mockAuthor({ uid: config.CXTIE_UID });
+    jest.spyOn(global.Math, "random").mockReturnValueOnce(0.99);
+
+    await mock.simulateEvent();
+
+    mock.expectRepliedSilentlyWith(sniffsEmoji.repeat(2));
+  });
+
+  it("should do nothing if neither condition is met", async () => {
+    const mock = new MockMessage(onSniffsSpec)
+      .mockContent("hello there")
+      .mockAuthor({ uid: config.CXTIE_UID });
+    await mock.simulateEvent();
+    mock.expectNotResponded();
   });
 });
