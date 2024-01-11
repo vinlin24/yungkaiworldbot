8259dde6eb52f43ca26261cdc8de6e851de14bc1
Vincent Lin <vinlin24@outlook.com>
2024-01-11 10:09:09 -0800

    Paginate dump of all cooldowns
    This solves the issue where our sheer number of cooldowns causes the
    plain text approach to exceed the 2000 character limit.

--- src/controllers/dev/cooldowns/list-cooldowns.command.ts
+++ src/controllers/dev/cooldowns/list-cooldowns.command.ts
@@ -1,4 +1,5 @@
-import { Events, SlashCommandBuilder } from "discord.js";
+import { pagination } from "@devraelfreeze/discordjs-pagination";
+import { EmbedBuilder, Events, SlashCommandBuilder, User } from "discord.js";
 
 import { BotClient } from "../../../bot/client";
 import getLogger from "../../../logger";
@@ -21,6 +22,11 @@ import { addBroadcastOption } from "../../../utils/options.utils";
 import { listenerIdAutocomplete } from "./cooldowns.autocomplete";
 
 const log = getLogger(__filename);
+
+/**
+ * Max characters for the description attribute of a Discord embed.
+ */
+const MAX_DESCRIPTION_LENGTH = 4096;
 
 function formatStatus(now: Date, expiration: Date): string {
   if (now >= expiration)
@@ -94,6 +100,41 @@ function formatListenerCooldownDump(
   return `__**${listener.id}** Cooldown__\n${formatted}`;
 }
 
+function splitIntoEmbedPages(
+  formattedDumpEntries: string[],
+  maxCharsPerPage: number = MAX_DESCRIPTION_LENGTH,
+): EmbedBuilder[] {
+  const embeds: EmbedBuilder[] = []
+  let description = "";
+  for (const entry of formattedDumpEntries) {
+    const { length } = entry;
+    if (length > maxCharsPerPage) {
+      log.warning( // TODO: not very helpful not knowing which dump lol.
+        `a formatted cooldown dump has length ${length} > ` +
+        `${maxCharsPerPage}, cannot fit it into an embed page, skipped.`
+      );
+      continue;
+    }
+    if (description.length + length > maxCharsPerPage) {
+      const embed = new EmbedBuilder()
+        .setTitle("Cooldown States")
+        .setDescription(description.trimEnd());
+      embeds.push(embed);
+      description = "";
+    }
+    description += entry + "\n";
+  }
+
+  if (description) {
+    const embed = new EmbedBuilder()
+      .setTitle("Cooldown States")
+      .setDescription(description.trimEnd());
+    embeds.push(embed);
+  }
+
+  return embeds;
+}
+
 const listCooldowns = new CommandBuilder();
 
 const slashCommandDefinition = new SlashCommandBuilder()
@@ -129,41 +170,48 @@ listCooldowns.execute(async (interaction) => {
     return;
   }
 
-  let response = "";
+  const context = formatContext(interaction);
   const now = new Date();
 
   // Caller provided a valid ID. Provide the dump for just that one listener.
   if (listenerId) {
     const listener = listenerMap.get(listenerId)!;
-    response = formatListenerCooldownDump(now, listener);
+    await interaction.reply({
+      content: formatListenerCooldownDump(now, listener),
+      ephemeral: !broadcast,
+      allowedMentions: { parse: [] }, // Suppress mention pinging.
+    });
+    log.debug(`${context}: dumped cooldown for ${listenerId}.`);
+    return;
   }
 
   // Otherwise provide the dumps for all listeners.
-  else {
-    const listeners = Array.from(listenerMap.values());
-    const entries = listeners.map(l => formatListenerCooldownDump(now, l));
-    response = entries.join("\n");
+  const listeners = Array.from(listenerMap.values());
+  const entries = listeners
+    .filter(l => l.cooldown?.type && l.cooldown.type !== "disabled")
+    .map(l => formatListenerCooldownDump(now, l));
 
-    if (!response) {
-      response = (
+  if (entries.length === 0) {
+    await interaction.reply({
+      content:
         "The bot isn't listening for anything that could have a cooldown " +
-        "at the moment!"
-      );
-    }
+        "at the moment!",
+      ephemeral: !broadcast,
+    });
+    return;
   }
 
-  await interaction.reply({
-    content: response,
+  const pages = splitIntoEmbedPages(entries, 600);
+  await pagination({
+    // @ts-expect-error. Embed constructor is private?? And you can't get Embed
+    // from EmbedBuilder?? I think under the hood, it uses .toJSON() to get
+    // APIEmbed, so leaving it as EmbedBuilder should be fine??
+    embeds: pages,
+    author: interaction.member!.user as User,
+    interaction,
     ephemeral: !broadcast,
-    allowedMentions: { parse: [] }, // Suppress mention pinging.
   });
-
-  const context = formatContext(interaction);
-  if (listenerId) {
-    log.debug(`${context}: dumped cooldown for ${listenerId}.`);
-  } else {
-    log.debug(`${context}: dumped cooldowns.`);
-  }
+  log.debug(`${context}: dumped cooldowns.`);
 });
 
 const listCooldownsSpec = listCooldowns.toSpec();
