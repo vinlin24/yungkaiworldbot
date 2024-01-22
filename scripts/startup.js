ccd0a55597cb08e34208c0a59d7c4bdf5e710d5a
Vincent Lin <vinlin24@outlook.com>
2024-01-21 18:43:58 -0800

    Make output of startup script show up in console

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { execSync } = require("node:child_process");
+const { spawnSync } = require("node:child_process");
 const path = require("node:path");
 
 const cwd = path.join(__dirname, "..");
 
-execSync("npm ci", { cwd });
-execSync("npm start", { cwd });
+spawnSync("npm ci", { cwd, shell: true, stdio: "inherit" });
+spawnSync("npm start", { cwd, shell: true, stdio: "inherit" });
