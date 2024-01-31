3a143af7b1c5b7e5994d9bd7f6277d57f64b355c
Vincent Lin <vinlin24@outlook.com>
2024-01-30 22:01:12 -0800

    Draft model for Settings collection

---
+++ src/models/settings.model.ts
@@ -0,0 +1,23 @@
+import { ActivityType } from "discord.js";
+import { Schema, model } from "mongoose";
+
+export type SettingKeys =
+  | "presence";
+
+export interface ISettings {
+  key: SettingKeys;
+  value: string;
+}
+
+export type PresenceEntry = {
+  activity_type?: keyof typeof ActivityType;
+  activity_name?: string;
+};
+
+const settingsSchema = new Schema<ISettings>({
+  key: { type: String, required: true, unique: true },
+  value: { type: String, required: true },
+});
+
+const SettingsModel = model("Settings", settingsSchema);
+export default SettingsModel;
