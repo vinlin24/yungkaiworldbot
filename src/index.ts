1b8118c72a695b3cfd12cbcbd40bfc68e69546fd
Vincent Lin <vinlin24@outlook.com>
2024-02-10 01:51:38 -0800

    Use argparse for parsing CLI options

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 // Program entry point.
 
+import { ArgumentParser, Namespace } from "argparse";
+
 import { BotClient } from "./bot/client";
 import env from "./config";
 import getLogger from "./logger";
 
 const log = getLogger(__filename);
 
+const parser = new ArgumentParser({
+  description: "Program entry point for the Discord bot of yung kai world.",
+});
+
+parser.add_argument("--sync", {
+  action: "store_true",
+  help: "deploy slash commands (do not start the bot runtime)",
+});
+parser.add_argument("--stealth", {
+  action: "store_true",
+  help: "start the bot runtime in stealth mode",
+});
+
 async function main() {
-  const stealth = process.argv.includes("--stealth");
+  const args = parser.parse_args() as Namespace;
+  const { sync, stealth } = args;
   const client = new BotClient(stealth);
 
-  if (process.argv.includes("--sync")) {
+  if (sync) {
     log.warning("deploying slash commands only...");
     await client.deploySlashCommands();
     return;
