71642caee221a6881e2a03e86522ed6760050c23
Vincent Lin <vinlin24@outlook.com>
2024-02-12 00:23:53 -0800

    Exit program on /shutdown success
    Previously, when terminating the bot with /shutdown, the bot instantly
    goes offline (since client.destroy() is called), but the process appears
    to hang in the terminal. Now we explicitly process.exit() at the end of
    the /shutdown callback.

--- tests/controllers/dev/shutdown.command.test.ts
+++ tests/controllers/dev/shutdown.command.test.ts
@@ -1,20 +1,38 @@
 import { BABY_MOD_RID } from "../../../src/config";
 import shutdownSpec from "../../../src/controllers/dev/shutdown.command";
-
 import { MockInteraction } from "../../test-utils";
 
 describe("/shutdown command", () => {
+  let exitSpy: jest.SpyInstance<never, [code?: number]>;
+  beforeEach(() => {
+    exitSpy = jest.spyOn(process, "exit").mockImplementation();
+  });
+
   it("should fail if privilege NONE (< BABY_MOD)", async () => {
     const mock = new MockInteraction(shutdownSpec);
+
     await mock.simulateCommand();
+
     expect(mock.interaction.client.destroy).not.toHaveBeenCalled();
+    expect(exitSpy).not.toHaveBeenCalled();
   });
 
   it("should destroy the client if authorized", async () => {
-    const mock = new MockInteraction(shutdownSpec);
-    mock.mockCallerRoles(BABY_MOD_RID);
+    const mock = new MockInteraction(shutdownSpec)
+      .mockCaller({ roleIds: [BABY_MOD_RID] });
+
     await mock.simulateCommand();
+
     expect(mock.interaction.client.destroy).toHaveBeenCalled();
     mock.expectRepliedWith({ content: "🫡" });
+  });
+
+  it("should exit the process with success status code", async () => {
+    const mock = new MockInteraction(shutdownSpec)
+      .mockCaller({ roleIds: [BABY_MOD_RID] });
+
+    await mock.simulateCommand();
+
+    expect(exitSpy).toHaveBeenCalledWith(0);
   });
 });
