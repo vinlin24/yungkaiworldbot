99bd51db0b5b4299936426c3da668fab820c6402
Vincent Lin <vinlin24@outlook.com>
2024-02-21 00:17:31 -0800

    Add tests for /set-concerted-react

---
+++ tests/controllers/dev/control/set-concerted-react.command.test.ts
@@ -0,0 +1,52 @@
+import { bold } from "discord.js";
+
+import { BOT_DEV_RID, KAI_RID } from "../../../../src/config";
+import setConcertedReactSpec from "../../../../src/controllers/dev/control/set-concerted-react.command";
+import { RoleLevel } from "../../../../src/middleware/privilege.middleware";
+import devControlService from "../../../../src/services/dev-control.service";
+import { MockInteraction } from "../../../test-utils";
+
+let mock: MockInteraction;
+beforeEach(() => { mock = new MockInteraction(setConcertedReactSpec); });
+
+it("should require privilege level >= DEV", async () => {
+  mock
+    .mockCaller({ roleIds: [KAI_RID] })
+    .mockOption("Boolean", "enabled", true);
+  jest.replaceProperty(devControlService, "reactWithDev", false);
+
+  await mock.simulateCommand();
+
+  expect(devControlService.reactWithDev).toEqual(false); // i.e. Unchanged.
+  mock.expectMentionedMissingPrivilege(RoleLevel.DEV);
+});
+
+it("should set the service state to be true", async () => {
+  mock
+    .mockCaller({ roleIds: [BOT_DEV_RID] })
+    .mockOption("Boolean", "enabled", true);
+  jest.replaceProperty(devControlService, "reactWithDev", false);
+
+  await mock.simulateCommand();
+
+  expect(devControlService.reactWithDev).toEqual(true);
+  mock.expectRepliedWith({
+    ephemeral: true,
+    content: `${bold("Enabled")} concerted DEV reactions.`,
+  });
+});
+
+it("should set the service state to be false", async () => {
+  mock
+    .mockCaller({ roleIds: [BOT_DEV_RID] })
+    .mockOption("Boolean", "enabled", false);
+  jest.replaceProperty(devControlService, "reactWithDev", true);
+
+  await mock.simulateCommand();
+
+  expect(devControlService.reactWithDev).toEqual(false);
+  mock.expectRepliedWith({
+    ephemeral: true,
+    content: `${bold("Disabled")} concerted DEV reactions.`,
+  });
+});
