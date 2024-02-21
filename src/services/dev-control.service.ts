4cb568a7426de1c8e1dc074626e77865cf3d9e48
Vincent Lin <vinlin24@outlook.com>
2024-02-20 23:29:07 -0800

    Add service and setter command for reactWithDev

---
+++ src/services/dev-control.service.ts
@@ -0,0 +1,6 @@
+export class DevControlService {
+  /** Whether the bot should perform concerted reactions with DEV reactions. */
+  public reactWithDev: boolean = false;
+}
+
+export default new DevControlService();
